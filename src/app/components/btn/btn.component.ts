745ed74529ce3f1ed17233cc99d7b69e8904fbe3
CristianCAgudeloC <camiloagdu@gmail.com>
2023-04-13 01:24:50 -0500

    Create view template LOGIN

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html'
+})
+export class BtnComponent implements OnInit{
+
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color = 'primary';
+
+  /**
+   *
+   */
+  constructor() {    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  get colors(){
+    return {
+      'bg-success-700 hover:bg-success-800 focus:ring-success-300': this.color === 'success',
+      'bg-primary hover:bg-violet-800 ': this.color === 'primary',
+      'bg-red-700 hover:bg-red-800 focus:ring-red-300': this.color === 'red',
+      
+    }
+
+  }
+}
